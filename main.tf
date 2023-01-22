d4a1b7ec18637eda3f7e2afc7c626d6902cdb8db
Yitaek Hwang <yitaek@gmail.com>
2023-01-22 11:47:23 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {}
+
+resource "digitalocean_droplet" "harperdb" {
+  image     = "ubuntu-18-04-x64"
+  name      = "harperdb"
+  region    = "nyc1"
+  size      = "s-1vcpu-1gb"
+  user_data = file("harperdb.yaml")
+}
+
+resource "digitalocean_firewall" "web" {
+  name = "harperdb"
+
+  droplet_ids = [digitalocean_droplet.harperdb.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9925-9926"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9926"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
